92b8f71420e321d2b251b2a898844837c239843c
Dan Singer <d_singer@fanshaweonline.ca>
2025-06-10 20:24:03 -0400

    Final Schedule Day
    -Added the final day to the schedule with timezone conversion

--- js/modules/schedule.js
+++ js/modules/schedule.js
@@ -11,53 +11,78 @@ export function schedulePopulation() {
         fetch(`${baseURL}panel/${d}`)
         .then(response => response.json())
         .then(function(response){
+            const colourBlindBox = document.querySelector("#colour-blind-box")
+            const features = document.querySelector("#schedule-features-con")
+
             scheduleImage.innerHTML = ""
             scheduleText.innerHTML = ""
 
-            let img = document.createElement("img")
-            img.src = `../images/schedule_images/day-${d}.png`
-            img.setAttribute("id", "schedule-image")
-            img.setAttribute("alt", `Image of Day ${d} Schedule`)
+            if (d !== "5") {
+                let img = document.createElement("img")
+                img.src = `../images/schedule_images/day-${d}.png`
+                img.setAttribute("id", "schedule-image")
+                img.setAttribute("alt", `Image of Day ${d} Schedule`)
 
-            scheduleImage.appendChild(img)
+                scheduleImage.appendChild(img)
 
-            response.forEach(panel => {
-                const panelTitle = document.createElement("p")
-                const panelTime = document.createElement("p")
-                const panelDescription = document.createElement("p")
-                const scheduleDiv = document.createElement("div")
-                const panelHost = document.createElement("p")
+                response.forEach(panel => {
+                    const panelTitle = document.createElement("p")
+                    const panelTime = document.createElement("p")
+                    const panelDescription = document.createElement("p")
+                    const scheduleDiv = document.createElement("div")
+                    const panelHost = document.createElement("p")
 
-                const utcTime = panel.time;
+                    const utcTime = panel.time;
+                    const utcDateTime = luxon.DateTime.fromFormat(utcTime, "yyyy-MM-dd HH:mm:ss", { zone: "utc" });
+
+                    const userZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+                    const localTime = utcDateTime.setZone(userZone).toLocaleString(luxon.DateTime.DATETIME_FULL);
+
+                    scheduleDiv.setAttribute("class", "panel-div")
+                    panelTitle.textContent = panel.title
+                    panelTitle.setAttribute("class", "panel-title")
+                    panelTime.textContent = `Time: ${localTime}`
+                    panelDescription.textContent = `Description: ${panel.description}`
+                    panelHost.textContent = `Hosted by: ${panel.host}`
+                    scheduleDiv.setAttribute("data-type", `${panel.type}`)
+
+                    if (panel.type === "m") {
+                        scheduleDiv.style.background = "linear-gradient( #0f1ca1, #01034c)"
+                    } else if (panel.type === "g") {
+                        scheduleDiv.style.background = "linear-gradient( #298933, #032404)"
+                    } else if (panel.type === "p") {
+                        scheduleDiv.style.background = "linear-gradient( #a78d2e, #523607)"
+                    } else if (panel.type === "v") {
+                        scheduleDiv.style.background = "linear-gradient( #95032d, #3d0003)"
+                    } 
+
+                    scheduleDiv.appendChild(panelTitle)
+                    scheduleDiv.appendChild(panelHost)
+                    scheduleDiv.appendChild(panelTime)
+                    scheduleDiv.appendChild(panelDescription)
+                    scheduleText.appendChild(scheduleDiv)
+
+                    colourBlindBox.style.display = "flex"
+                    features.style.display = "flex"
+                })
+            } else {
+                const finalDiv = document.createElement("div")
+                const p = document.createElement("p")
+
+                const utcTime = "2025-05-04 15:00:00";
                 const utcDateTime = luxon.DateTime.fromFormat(utcTime, "yyyy-MM-dd HH:mm:ss", { zone: "utc" });
 
                 const userZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
                 const localTime = utcDateTime.setZone(userZone).toLocaleString(luxon.DateTime.DATETIME_FULL);
 
-                scheduleDiv.setAttribute("class", "panel-div")
-                panelTitle.textContent = panel.title
-                panelTitle.setAttribute("class", "panel-title")
-                panelTime.textContent = `Time: ${localTime}`
-                panelDescription.textContent = `Description: ${panel.description}`
-                panelHost.textContent = `Hosted by: ${panel.host}`
-                scheduleDiv.setAttribute("data-type", `${panel.type}`)
+                p.innerHTML = `<p id="closing-info">EmblemCon's Closing Ceremonies will be held on ${localTime}<br>Please feel free to join us for wrap ups, final thoughts, and the yearly chaos!`
 
-                if (panel.type === "m") {
-                    scheduleDiv.style.background = "linear-gradient( #0f1ca1, #01034c)"
-                } else if (panel.type === "g") {
-                    scheduleDiv.style.background = "linear-gradient( #298933, #032404)"
-                } else if (panel.type === "p") {
-                    scheduleDiv.style.background = "linear-gradient( #a78d2e, #523607)"
-                } else if (panel.type === "v") {
-                    scheduleDiv.style.background = "linear-gradient( #95032d, #3d0003)"
-                } 
+                finalDiv.appendChild(p)
+                scheduleText.appendChild(finalDiv)
 
-                scheduleDiv.appendChild(panelTitle)
-                scheduleDiv.appendChild(panelHost)
-                scheduleDiv.appendChild(panelTime)
-                scheduleDiv.appendChild(panelDescription)
-                scheduleText.appendChild(scheduleDiv)
-            })
+                colourBlindBox.style.display = "none"
+                features.style.display = "none"
+            }
         })
         .catch(error => {
             const errorMessage = document.createElement("p")
